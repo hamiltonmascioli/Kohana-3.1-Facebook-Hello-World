fb93746ed635b79e842abcd8ee96fa433a73fe03
Jason Hamilton-Mascioli <hamiltonmascioli@gmail.com>
2014-08-09 09:45:52 -0400

    Update facebook.php

--- modules/hops/config/facebook.php
+++ modules/hops/config/facebook.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-   'app_id'          => '150254088381735',
-   'api_key'         => 'e949f6cc1de236c1ddcf11d757ee201c',
-   'secret'          => '2b3517d6401a266617a5c444165cbabf',
+   'app_id'          => '',
+   'api_key'         => '',
+   'secret'          => '',
 );
